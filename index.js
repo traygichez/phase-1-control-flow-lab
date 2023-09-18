1cb63da9482daaff595502a594f3dfa4f1acac10
tray <onesimus.githua@student.moringaschool.com>
2023-09-18 20:25:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(x) {
+  if(x < 400) {
+    return 'This one is on me!'
+  }
+  else if(x >2000&& x<=2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
+}
+function ternaryCheckCity(b) {
+  if (b === 'NYC') {
+    return 'Ok, sounds good.';
+  } else {
+    return 'No go.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t) {
+  switch (t) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
